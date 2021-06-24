ad4051263931c1c3363a483c59376a8c68de6371
Anil Nekuri <anilnekuri@gmail.com>
2021-06-24 07:45:57 +0530

    insertion

--- data-structures/src/main/java/com/dsguy/datastructures/array/SingleDimensionArray.java
+++ data-structures/src/main/java/com/dsguy/datastructures/array/SingleDimensionArray.java
@@ -2,6 +2,9 @@ package com.dsguy.datastructures.array;
 
 import java.util.Arrays;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SingleDimensionArray {
 	int intArray[] = null;
 	
@@ -16,6 +19,18 @@ public class SingleDimensionArray {
 		.forEach(System.out::print);
 	}
 
-	
+	public void insert(int location, int valueToBeInserted) {
+		try {
+			if(intArray[location] == Integer.MIN_VALUE) {
+				intArray[location] = valueToBeInserted;
+				log.debug("Successfully inserted "+valueToBeInserted+" at location "+location);
+				log.debug("Successfully inserted {} at location {}.",valueToBeInserted, location);
+			}else {
+				log.debug("This cell is already occupied with another value.");
+			}
+		}catch(ArrayIndexOutOfBoundsException e) {
+			log.error("Invlid index",e);
+		}
+	}
 	
 }
